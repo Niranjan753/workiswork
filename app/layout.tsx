c0b0692660e38cff456d6b57acfe3c98c9b9e75c
Niranjan753 <niranjanr753@gmail.com>
2026-01-06 21:22:28 +0530

    added meta header in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,6 +55,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Primary Meta Tags */}
+        <title>Meta Tags — Preview, Edit and Generate</title>
+        <meta name="title" content="Meta Tags — Preview, Edit and Generate" />
+        <meta
+          name="description"
+          content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, X and more!"
+        />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://workiswork.xyz" />
+        <meta property="og:title" content="Meta Tags — Preview, Edit and Generate" />
+        <meta
+          property="og:description"
+          content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, X and more!"
+        />
+        <meta property="og:image" content="/Users/berlin/workiswork/workiswork/public/metaheader.png" />
+
+        {/* X (Twitter) */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://workiswork.xyz" />
+        <meta property="twitter:title" content="Meta Tags — Preview, Edit and Generate" />
+        <meta
+          property="twitter:description"
+          content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, X and more!"
+        />
+        <meta property="twitter:image" content="/Users/berlin/workiswork/workiswork/public/metaheader.png" />
+        {/* Meta Tags Generated with https://metatags.io */}
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-zinc-950 text-zinc-50`}
       >
